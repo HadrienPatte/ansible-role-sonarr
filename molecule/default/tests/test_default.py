d393e1f0de86454f31f14539913a63f3b3ebcf38
Hadrien Patte <hadrien.patte@protonmail.com>
2020-02-10 02:13:19 +0100

    Install sonarr
    Signed-off-by: Hadrien Patte <hadrien.patte@protonmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -2,12 +2,16 @@ import os
 
 import testinfra.utils.ansible_runner
 
+import pytest
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+@pytest.mark.parametrize('name', [
+    ('apt-transport-https'),
+    ('sonarr'),
+])
+def test_package_is_installed(host, name):
+    package = host.package(name)
+    assert package.is_installed
