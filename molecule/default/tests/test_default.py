e7a5d9cf0dfd8de9b0f2a1bc20d63b39c43c5ff0
Hadrien Patte <hadrien.patte@protonmail.com>
2020-02-10 02:15:48 +0100

    Enable sonarr service
    Signed-off-by: Hadrien Patte <hadrien.patte@protonmail.com>

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,3 +15,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 def test_package_is_installed(host, name):
     package = host.package(name)
     assert package.is_installed
+
+
+@pytest.mark.parametrize('name', [
+    ('sonarr'),
+])
+def test_service_is_running(host, name):
+    service = host.service(name)
+    assert service.is_running
+
+
+@pytest.mark.parametrize('name', [
+    ('sonarr'),
+])
+def test_service_is_enabled(host, name):
+    service = host.service(name)
+    assert service.is_enabled
+
+
+@pytest.mark.parametrize('port', [
+    ('8989'),
+])
+def test_socket(host, port):
+    assert host.socket('tcp://0.0.0.0:' + port).is_listening
